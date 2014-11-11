7c8a0eda5d35e83eb1945a4be78bed8e5763ce2a
molenzwiebel <info@thijsmolendijk.nl>
2014-11-11 21:29:52 +0100

    Some stuff in OPGMMatch Too tired to work on this more for now, will be completed later

--- src/main/java/me/itsmiiolly/ollypgm/match/OPGMMatch.java
+++ src/main/java/me/itsmiiolly/ollypgm/match/OPGMMatch.java
@@ -1,22 +1,28 @@
 package me.itsmiiolly.ollypgm.match;
 
+import java.util.Collection;
+
+import me.itsmiiolly.ollypgm.OPGMPlayer;
 import me.itsmiiolly.ollypgm.map.OPGMMap;
 import me.itsmiiolly.ollypgm.module.OPGMModule;
 import me.itsmiiolly.ollypgm.module.OPGMModuleManager;
 import me.itsmiiolly.ollypgm.module.OPGMModuleRegistry;
 
+import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 
 public class OPGMMatch {
     private Plugin plugin;
+    private World matchWorld;
     
     private OPGMMap map;
     private OPGMModuleManager moduleManager;
     
-    public OPGMMatch(OPGMMap map, Plugin plugin, OPGMModuleRegistry moduleRegistry) {
+    public OPGMMatch(OPGMMap map, World world, Plugin plugin, OPGMModuleRegistry moduleRegistry) {
         try {
             this.plugin = plugin;
             this.map = map;
+            this.matchWorld = world;
             
             this.moduleManager = new OPGMModuleManager(this, map.getDocument(), moduleRegistry);
         } catch (Exception ex) {
@@ -36,6 +42,20 @@ public class OPGMMatch {
      */
     public Plugin getPlugin() {
         return this.plugin;
+    }
+    
+    /**
+     * @return the world this match is playing in
+     */
+    public World getMatchWorld() {
+        return this.matchWorld;
+    }
+    
+    /**
+     * @return every player participating in the match, regardless if they are observing or playing
+     */
+    public Collection<OPGMPlayer> getParticipatingPlayers() {
+        return OPGMPlayer.getPlayers(this);
     }
     
     /**
